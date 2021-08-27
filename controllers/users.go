dc621f875ab25cee70260df496717d7fd47f1f8c
monkjunior <vungocson998@gmail.com>
2021-08-27 15:38:19 +0700

    Completed login flow

--- controllers/users.go
+++ controllers/users.go
@@ -1,10 +1,8 @@
 package controllers
 
 import (
-	"log"
 	"net/http"
 
-	"github.com/gorilla/schema"
 	"github.com/monkjunior/poc-kratos-hydra/kratos"
 	"github.com/monkjunior/poc-kratos-hydra/views"
 	kratosClient "github.com/ory/kratos-client-go"
@@ -40,38 +38,45 @@ type Users struct {
 	kratosClient     *kratosClient.APIClient
 }
 
+// LoginForm stores data for rendering Login form and submit a Login flow
+type LoginForm struct {
+	SubmitMethod       string
+	Action             string
+	CsrfToken          string `schema:"csrf_token"`
+	FlowID             string
+	Email              string `schema:"password_identifier"`
+	Password           string `schema:"password"`
+}
+
+// GetLogin requires flow params, if the flow is not set, it will redirect to Kratos to browse a new one.
+// Kratos will create a new flow and redirect back to /auth/login with the param was set in the URL.
+// GetLogin will use this id to fetch data from Kratos to render submit form.
+//
+// GET /auth/login/?flow=<flow_id>
 func (u *Users) GetLogin(w http.ResponseWriter, r *http.Request) {
 	flow := r.URL.Query().Get("flow")
 	if flow == "" {
 		http.Redirect(w, r, KratosPublicBaseURL+"/self-service/login/browser", http.StatusFound)
 		return
 	}
-	u.LoginView.Render(w, r, nil)
-}
-
-type LoginForm struct {
-	Email    string `schema:"email"`
-	Password string `schema:"password"`
-	Remember bool   `schema:"remember"`
-}
-
-func (u *Users) PostLogin(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		log.Println(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	flowObject, res, err := u.kratosClient.V0alpha1Api.GetSelfServiceLoginFlow(r.Context()).Id(flow).Cookie(r.Header.Get("Cookie")).Execute()
+	if err != nil || res == nil || res.StatusCode != http.StatusOK {
+		kratos.LogOnError(err, res)
+		return
 	}
-	var form LoginForm
-	dec := schema.NewDecoder()
-	dec.IgnoreUnknownKeys(true)
-	if err := dec.Decode(&form, r.PostForm); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	kratos.PrintJSONPretty(flowObject)
+	data := views.Data{
+		Yield: RegistrationForm{
+			CsrfToken:    flowObject.Ui.GetNodes()[0].Attributes.UiNodeInputAttributes.Value.(string),
+			FlowID:       flow,
+			SubmitMethod: flowObject.Ui.Method,
+			Action:       flowObject.Ui.Action,
+		},
 	}
-	log.Println(form)
-	// TODO: implement login
-	//http.Redirect(w, r, KratosPublicBaseURL + "/self-service/login/browser", http.StatusFound)
+	u.LoginView.Render(w, r, data)
 }
 
+// RegistrationForm stores data for rendering Registration form and submit a Registration flow
 type RegistrationForm struct {
 	RegistrationMethod string
 	SubmitMethod       string
@@ -82,6 +87,12 @@ type RegistrationForm struct {
 	Password           string `schema:"password"`
 }
 
+// GetRegistration requires flow params to render Registration screen
+// if flow param is not found, it will redirect to Kratos /self-service/registration/browser
+// to browse a new flow_id.
+// Kratos then redirect back to this path with a flow param in the URL.
+//
+// GET /auth/registration/?flow=<flow_id>
 func (u *Users) GetRegistration(w http.ResponseWriter, r *http.Request) {
 	flow := r.URL.Query().Get("flow")
 	if flow == "" {
--- main.go
+++ main.go
@@ -19,7 +19,6 @@ func main() {
 	r := mux.NewRouter()
 	r.Handle("/", logMw.Apply(protectedSites.Dashboard))
 	r.HandleFunc("/auth/login", logMw.ApplyFn(userC.GetLogin)).Methods("GET")
-	r.HandleFunc("/auth/login", logMw.ApplyFn(userC.PostLogin)).Methods("POST")
 	r.HandleFunc("/auth/registration", logMw.ApplyFn(userC.GetRegistration)).Methods("GET")
 	fmt.Println("Listening at port 4435 ...")
 	log.Fatal(http.ListenAndServe(":4435", r))
