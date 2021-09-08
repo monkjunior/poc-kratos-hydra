2c2d7d8e3eb187dd1bc6a3719a2968a5f7228d56
monkjunior <vungocson998@gmail.com>
2021-09-08 12:05:54 +0700

    Handled /callback error.

--- controllers/users.go
+++ controllers/users.go
@@ -120,37 +120,78 @@ func (u *Users) GetRegistration(w http.ResponseWriter, r *http.Request) {
 
 // CallbackForm stores result token after OAuth flow
 type CallbackForm struct {
+	Error        CallbackError
 	AccessToken  string
 	RefreshToken string
 	Expiry       string
 	IDToken      string
 }
 
+type CallbackError struct {
+	Name        string
+	Description string
+	Hint        string
+	Debug       string
+}
+
 // GetCallback receive authorization code and exchange token with Hydra, our OAuth2.0/OIDC server
 // then it render token, and other result to viewer.
 // GET /callback
 func (u *Users) GetCallback(w http.ResponseWriter, r *http.Request) {
-	if len(r.URL.Query().Get("error")) == 0 {
-		// TODO: validate if states is matched
-		if true {
-			code := r.URL.Query().Get("code")
-			token, err := exchangeToken(r.Context(), code)
-			if err == nil {
-				idt := token.Extra("id_token")
-				data := views.Data{
-					Yield: CallbackForm{
-						AccessToken:  token.AccessToken,
-						RefreshToken: token.RefreshToken,
-						Expiry:       token.Expiry.Format(time.RFC1123),
-						IDToken:      fmt.Sprintf("%v", idt),
-					},
-				}
-				u.CallbackView.Render(w, r, data)
-				return
-			}
-			fmt.Fprintln(w, "EXCHANGE TOKEN FAILED", err.Error())
-			return
+	if len(r.URL.Query().Get("error")) > 0 {
+		data := views.Data{
+			Yield: CallbackForm{
+				Error: CallbackError{
+					Name:        r.URL.Query().Get("error"),
+					Description: r.URL.Query().Get("error_description"),
+					Hint:        r.URL.Query().Get("error_hint"),
+					Debug:       r.URL.Query().Get("error_debug"),
+				},
+			},
 		}
+		u.CallbackView.Render(w, r, data)
+		return
 	}
-	fmt.Fprintln(w, "GOT ERROR")
+
+	// TODO: validate if states is matched
+	isStatesMatched := true
+	if !isStatesMatched {
+		data := views.Data{
+			Yield: CallbackForm{
+				Error: CallbackError{
+					Name:        "States does not match",
+					Description: "Expect A but received B",
+				},
+			},
+		}
+		u.CallbackView.Render(w, r, data)
+		return
+	}
+
+	code := r.URL.Query().Get("code")
+	token, err := exchangeToken(r.Context(), code)
+	if err != nil {
+		data := views.Data{
+			Yield: CallbackForm{
+				Error: CallbackError{
+					Name:        "Failed to exchange token",
+					Description: err.Error(),
+				},
+			},
+		}
+		u.CallbackView.Render(w, r, data)
+		return
+	}
+
+	idToken := token.Extra("id_token")
+	data := views.Data{
+		Yield: CallbackForm{
+			AccessToken:  token.AccessToken,
+			RefreshToken: token.RefreshToken,
+			Expiry:       token.Expiry.Format(time.RFC1123),
+			IDToken:      fmt.Sprintf("%v", idToken),
+		},
+	}
+	u.CallbackView.Render(w, r, data)
+	return
 }
